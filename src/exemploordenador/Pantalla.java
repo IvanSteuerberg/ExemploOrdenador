5dab9ce29c3ca793dc3fb65ecfe48a7bcdf49c6f
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2019-10-25 13:43:06 +0200

    Clase Pantalla comentada

--- src/exemploordenador/Pantalla.java
+++ src/exemploordenador/Pantalla.java
@@ -1,3 +1,7 @@
+/**
+ * Clase Pantalla utilizada para crear objetos(pantallas) que forman
+ * parte de un ordenador
+ */
 package exemploordenador;
 // @author irodriguezsteuerberg
 
@@ -10,15 +14,31 @@ public Pantalla(String marca, float pulgadas){
 this.marca=marca;
 this.pulgadas=pulgadas;
 }
+/**
+ * Metedo para darle valor a marca
+ * @param marca 
+ */
 public void setMarca(String marca){
 this.marca=marca;    
 }
+/**
+ * Metodo para obtener el valor de marca
+ * @return 
+ */
 public String getMarca(){
 return marca;    
 }
+/**
+ * Metodo para darle valor a pulgadas
+ * @param pulgadas 
+ */
 public void setPulgadas(float pulgadas){
 this.pulgadas=pulgadas;    
 }
+/**
+ * Metodo para obtener el valor de pulgadas
+ * @return 
+ */
 public float getPulgadas(){
 return pulgadas;    
 }
