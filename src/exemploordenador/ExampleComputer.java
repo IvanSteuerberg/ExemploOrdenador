d5e8333a4a877f5474014945d21ed03083bcb744
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2019-11-06 14:28:22 +0100

    Clase ExemploOrdenador con javadoc en ingles

---
+++ src/exemploordenador/ExampleComputer.java
@@ -0,0 +1,38 @@
+package exemploordenador;
+
+//@author irodriguezsteuerberg
+import java.util.Scanner;
+
+/**
+ * Main class ExampleComputer
+ *
+ * @author irodriguezsteuerberg
+ */
+public class ExampleComputer {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Computer pc = new Computer();
+        pc.see();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce o tipo de rato:");
+        String tipo = sc.next();
+        System.out.println("Introduce a marca da pantalla:");
+        String brand = sc.next();
+        System.out.println("Introduce as pulgadas:");
+        float inches = sc.nextFloat();
+        System.out.println("Introduce a velocidade do procesador (en MHz):");
+        int speed = sc.nextInt();
+        System.out.println("Introduce a memoria do procesador (en GB):");
+        int memory = sc.nextInt();
+        Mouse mouse = new Mouse(tipo);
+        Screen screen = new Screen(brand, inches);
+        Cpu cpu = new Cpu(speed, memory);
+        Computer pc2 = new Computer(mouse, cpu, screen);
+        pc2.see();
+    }
+
+}
