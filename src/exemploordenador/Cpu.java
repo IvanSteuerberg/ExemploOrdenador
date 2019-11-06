ece7fa9cdc0b0cf659691b0c89b24f579bb6bca8
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2019-11-06 14:13:18 +0100

    Clase Cpu con javadoc en ingles

--- src/exemploordenador/Cpu.java
+++ src/exemploordenador/Cpu.java
@@ -1,27 +1,62 @@
 package exemploordenador;
 //@author irodriguezsteuerberg
- 
-public class Cpu {
-private int velocidade;
-private int memoria;
-public Cpu(){    
-}
-public Cpu(int velocidade, int memoria){
-this.velocidade=velocidade;
-this.memoria=memoria;
-}
-public void setVelocidade(int velocidade){
-this.velocidade=velocidade;    
-}
-public int getVelocidade(){
-return velocidade;    
-}
-public void setMemoria(int memoria){
-this.memoria=memoria;    
-}
-public int getMemoria(){
-return memoria;    
-}
 
+/**
+ * class Mouse used to create objects(cpus) used to create a computer
+ * @author irodriguezsteuerberg
+ */
+
+public class Cpu {
+
+    private int speed;
+    private int memory;
+
+    /**
+     * Constructor predetermined for the objects of the class Cpu
+     */
+    public Cpu() {
+    }
+
+    /**
+     * Constructor with parameters for the objects of the class Cpu
+     * @param speed of the cpu
+     * @param memory size of the memory available
+     */
+    public Cpu(int speed, int memory) {
+        this.speed = speed;
+        this.memory = memory;
+    }
+
+    /**
+     * Method used to set the value of speed
+     * @param speed of the cpu
+     */
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    /**
+     * Method used to get the value of speed
+     * @return speed of the cpu
+     */
+    public int getSpeed() {
+        return speed;
+    }
+
+    /**
+     * Method used to set the value of memory
+     * @param memory of the cpu
+     */
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    /**
+     * Method used to get the value of memory
+     * @return memory of the cpu
+     */
+    public int getMemory() {
+        return memory;
+    }
 
 }
